627ec63ca08fc6dcc5b1a158dea97a8e01131656
Will Clemens <waclemens@anderson.edu>
2020-10-01 13:06:00 -0400

    small formating changes

--- connectToBlue.js
+++ connectToBlue.js
@@ -1,24 +1,24 @@
 const { createBluetooth } = require('node-ble')
 
 async function main () {
-	console.log('start');
-	const { bluetooth, destroy } = createBluetooth();
-	const adapter = await bluetooth.defaultAdapter();
+  console.log('start');
+  const { bluetooth, destroy } = createBluetooth();
+  const adapter = await bluetooth.defaultAdapter();
 
-	console.log('got adapter');
-	console.log(adapter);
+  console.log('got adapter');
+  console.log(adapter);
 
-	if (! await adapter.isDiscovering()) {
-		await adapter.startDiscovery();
-	}
+  if (! await adapter.isDiscovering()) {
+    await adapter.startDiscovery();
+  }
 
-	console.log('start searching for bluetooth device');
+  console.log('start searching for bluetooth device');
 
-	const device = await adapter.waitDevice('90:84:2B:18:B1:A8');
-	await device.connect();
+  const device = await adapter.waitDevice('90:84:2B:18:B1:A8');
+  await device.connect();
 
-	console.log('connected');
-	console.log(device);
+  console.log('connected');
+  console.log(device);
 }
 
 main()
