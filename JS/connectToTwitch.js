3185c67d0db4697c0c0ab9290dcf9b1a7016cfd5
Noah Schram <nsschram@anderson.edu>
2020-10-06 14:29:09 -0400

    fix all documentation issues

--- JS/connectToTwitch.js
+++ JS/connectToTwitch.js
@@ -1,52 +1,50 @@
 'use strict'
 // import the js library needed to work in twitch
-const tmi = require('tmi.js');
+const tmi = require('tmi.js')
 
-//basic username and channel name for our specfic project Oauth_token is found in the config file
-const BOT_USERNAME = "twitchpoweredup";
-const OAUTH_TOKEN = "";
-const CHANNEL_NAME = "twitchpoweredup";
+// basic username and channel name for our specfic project Oauth_token is found in the config file
+const BOT_USERNAME = 'twitchpoweredup'
+const OAUTH_TOKEN = ''
+const CHANNEL_NAME = 'twitchpoweredup'
 
-function main(){
-    connectToTwitch();
+function main () {
+  connectToTwitch()
 }
 
-function connectToTwitch(){
-    const connectionObj = {
-        identity: {
-        username: BOT_USERNAME,
-        password: OAUTH_TOKEN
-        },
-        channels: [
-        CHANNEL_NAME
-        ]
-    }; 
+function connectToTwitch () {
+  const connectionObj = {
+    identity: {
+      username: BOT_USERNAME,
+      password: OAUTH_TOKEN
+    },
+    channels: [
+      CHANNEL_NAME
+    ]
+  }
 
-    const client = new tmi.client(connectionObj);
+  const client = new tmi.Client(connectionObj)
 
-    // Registers our event handlers
-    client.on('message', onMessageHandler);
-    client.on('connected', onConnectedHandler);
+  // Registers our event handlers
+  client.on('message', onMessageHandler)
+  client.on('connected', onConnectedHandler)
 
-    // Connect to Twitch
-    client.connect();
+  // Connect to Twitch
+  client.connect()
 }
 
 function onMessageHandler (target, context, msg, self) {
-    // Ignore messages from the bot such as shout messages for user commands
-    if (self) { return; } 
-  
-    // Remove whitespace from chat message
-    const commandName = msg.trim();
+  // Ignore messages from the bot such as shout messages for user commands
+  if (self) { return }
 
-    
-    console.log(commandName);
-    
+  // Remove whitespace from chat message
+  const commandName = msg.trim()
+
+  console.log(commandName)
 }
 
-//shows that we have connected to the twitch account
+// shows that we have connected to the twitch account
 function onConnectedHandler (addr, port) {
-    console.log(`* Connected to ${addr}:${port}`);
+  console.log(`* Connected to ${addr}:${port}`)
 }
 
-main();
+main()
