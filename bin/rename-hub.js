6adc6f36fddc8212f77033581c1edc5620b524b7
Will <waclemens@anderson.edu>
2020-11-20 11:58:49 -0500

    remove eslint-disable no-var

--- bin/rename-hub.js
+++ bin/rename-hub.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-/* eslint-disable no-var */
 
 const renameHub = require('../src/renameHub.js')
 renameHub.init()
--- bin/twitch-powered-up.js
+++ bin/twitch-powered-up.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-/* eslint-disable no-var */
 
 const twitchPoweredUp = require('../src/twitchPoweredUp.js')
 twitchPoweredUp.init()
