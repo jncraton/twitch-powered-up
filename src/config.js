452bbc662cedd3329c13b79ea9c7f9a7f166f2ce
Jon Craton <jncraton@gmail.com>
2020-12-01 11:30:12 -0500

    Use jsonschema directly for validation

--- src/config.js
+++
@@ -1,5 +0,0 @@
-const validate = require('jsonschema').validate;
-const schema = require('./config.schema.json')
-const config = require('../examples/exampleConfig.json')
-
-console.log(validate(config, schema))
--- src/twitchPoweredUp.js
+++ src/twitchPoweredUp.js
@@ -3,13 +3,25 @@ const bluetooth = require('./bluetooth')
 const fs = require('fs')
 const stream = require('./stream')
 const xdgBasedir = require('xdg-basedir')
+const jsonschema = require('jsonschema')
 const configPath = xdgBasedir.config + '/twitch-powered-up.json'
 const examplePath = __dirname.replace(/\\/g, '/').replace(/\/src$/, '/examples/exampleConfig.json')
+const schema = require('./config.schema.json')
+
 let config
 
 const init = async () => {
   config = await getConfig()
-  await checkValidConfig()
+
+  try {
+    jsonschema.validate(config, schema, { throwAll: true })
+  } catch (validationError) {
+    console.log('There were errors found in your config file:')
+    validationError.errors.forEach(e => {
+      console.log(e.stack.replace('instance.', ''))
+    })
+    process.exit(0)
+  }
   twitch.connect(onMessageHandler, onConnectedHandler, config)
   bluetooth.startScan()
   stream.start(config)
@@ -51,59 +63,6 @@ const getConfig = () => {
           process.exit(0)
         }
       })
-    }
-  })
-}
-
-const checkValidConfig = () => {
-  return new Promise(resolve => {
-    const configErrors = []
-
-    if (!config) { configErrors.push('config not found...') }
-    if (config && !config.twitch) { configErrors.push('config.twitch not found...') }
-    if (config && config.twitch && !config.twitch.auth) { configErrors.push('config.twitch.auth not found...') }
-    if (config && config.twitch && !config.twitch.refresh) { configErrors.push('config.twitch.refresh not found...') }
-    if (config && config.twitch && !config.twitch.username) { configErrors.push('config.twitch.username not found...') }
-    if (config && config.twitch && !config.twitch.channel) { configErrors.push('config.twitch.channel not found...') }
-
-    if (config && config.twitch && !config.twitch.stream) { configErrors.push('config.twitch.stream not found...') }
-    if (config && config.twitch && config.twitch.stream && !config.twitch.stream.streamKey) { configErrors.push('config.twitch.stream.streamKey not found...') }
-    if (config && config.twitch && config.twitch.stream && !config.twitch.stream.ingestServer) { configErrors.push('config.twitch.stream.ingestServer not found...') }
-    if (config && config.twitch && config.twitch.stream && !config.twitch.stream.displayDevice) { configErrors.push('config.twitch.stream.displayDevice not found...') }
-    if (config && config.twitch && config.twitch.stream && !config.twitch.stream.framerate) { configErrors.push('config.twitch.stream.framerate not found...') }
-    if (config && config.twitch && config.twitch.stream && !config.twitch.stream.qualityPreset) { configErrors.push('config.twitch.stream.qualityPreset not found...') }
-
-    if (config && !config.devices) { configErrors.push('config.devices not found...') }
-    if (config && config.devices && config.devices.length === 0) { configErrors.push('config.devices has no devices...') }
-
-    if (config && config.devices) {
-      config.devices.forEach((device, deviceIndex) => {
-        if (!device.hub) { configErrors.push('config.device[' + deviceIndex + '].hub not found...') }
-        if (!device.port) { configErrors.push('config.device[' + deviceIndex + '].port not found...') }
-        if (!device.nouns) { configErrors.push('config.device[' + deviceIndex + '].nouns not found...') }
-        if (!device.actions) { configErrors.push('config.device[' + deviceIndex + '].actions not found...') }
-        if (device.nouns && device.nouns.length === 0) { configErrors.push('config.device[' + deviceIndex + '].actions has no nouns...') }
-        if (device.actions && device.actions.length === 0) { configErrors.push('config.device[' + deviceIndex + '].actions has no actions...') }
-
-        if (device.actions) {
-          device.actions.forEach((action, actionIndex) => {
-            if (!action.method) { configErrors.push('config.device[' + deviceIndex + '].actions[' + actionIndex + '].method not found...') }
-            if (!action.verbs) { configErrors.push('config.device[' + deviceIndex + '].actions[' + actionIndex + '].verbs not found...') }
-            if (typeof action.value === 'undefined') { configErrors.push('config.device[' + deviceIndex + '].actions[' + actionIndex + '].value not found...') }
-            if (action.verbs && action.verbs.length === 0) { configErrors.push('config.device[' + deviceIndex + '].actions[' + actionIndex + '].verbs has no verbs...') }
-          })
-        }
-      })
-    }
-
-    if (configErrors.length > 0) {
-      configErrors.forEach((error) => {
-        console.log(error)
-      })
-      console.log(configPath + ' is not set up properly...')
-      process.exit(1)
-    } else {
-      resolve('done')
     }
   })
 }
