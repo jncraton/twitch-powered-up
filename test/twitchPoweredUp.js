d9cdb25d26a7ca1c7525457222bdb901e51e031d
Will <waclemens@anderson.edu>
2020-12-01 12:27:08 -0500

    remove assert

--- test/twitchPoweredUp.js
+++ test/twitchPoweredUp.js
@@ -3,8 +3,7 @@ const twitchPoweredUp = require('../src/twitchPoweredUp')
 const config = require('../examples/exampleConfig.json')
 
 const test = async () => {
-  const resolve = await twitchPoweredUp.checkValidConfig(config)
-  assert.equal(resolve, 'good')
+  await twitchPoweredUp.checkValidConfig(config)
 }
 
 test()
