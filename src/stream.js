b5648bbbead4a8ff6cf39924fde3a7133465d039
Jon Craton <jncraton@gmail.com>
2020-12-08 14:53:17 -0500

    Remove newlines from command

--- src/stream.js
+++ src/stream.js
@@ -3,27 +3,25 @@
 const { exec } = require('child_process')
 
 function start (config) {
-  const cmdString = `
-    ffmpeg 
-      -loglevel panic 
-      -f lavfi 
-      -i anullsrc=channel_layout=stereo:sample_rate=44100 
-      -f v4l2 
-      -video_size ${config.twitch.stream.videoSize} 
-      -i ${config.twitch.stream.displayDevice}
-      -fflags nobuffer 
-      -tune zerolatency 
-      -vcodec libx264 
-      -r ${config.twitch.stream.framerate} 
-      -vf ${config.twitch.stream.videoFilter}
-      -g 30
-      -pix_fmt yuv420p
-      -preset ${config.twitch.stream.qualityPreset} 
-      -b:v ${config.twitch.stream.bitrate} 
-      -force_key_frames 0:00:02 
-      -f flv 
-      "rtmp://${config.twitch.stream.ingestServer}.twitch.tv/app/${config.twitch.stream.streamKey}"
-  `
+  const cmdString = `ffmpeg \
+    -loglevel panic \
+    -f lavfi \
+    -i anullsrc=channel_layout=stereo:sample_rate=44100 \
+    -f v4l2 \
+    -video_size ${config.twitch.stream.videoSize} \
+    -i ${config.twitch.stream.displayDevice} \
+    -fflags nobuffer \
+    -tune zerolatency \
+    -vcodec libx264 \
+    -r ${config.twitch.stream.framerate} \
+    -vf ${config.twitch.stream.videoFilter} \
+    -g 30 \
+    -pix_fmt yuv420p \
+    -preset ${config.twitch.stream.qualityPreset} \
+    -b:v ${config.twitch.stream.bitrate} \
+    -force_key_frames 0:00:02 \
+    -f flv \
+    "rtmp://${config.twitch.stream.ingestServer}.twitch.tv/app/${config.twitch.stream.streamKey}"`
 
   const startStream = exec(cmdString, (error, stdout, stderr) => {
     if (error) {
