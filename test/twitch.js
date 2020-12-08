7c37ba2673d38820c2ca404f16030ccc29feb682
Amberlie Jock <44444108+AmberlieJock@users.noreply.github.com>
2020-12-08 09:28:11 -0500

    Test how the running average works

--- test/twitch.js
+++ test/twitch.js
@@ -4,11 +4,21 @@ const config = require('../examples/exampleConfig.json')
 
 const test = () => {
   const token = twitch.actionTokenFromMessage('Move the red train forward at power 30', config)
-
+  twitch.update()
   assert.equal(token.hub, 'hub1')
   assert.equal(token.port, 'A')
   assert.equal(token.method, 'setPower')
   assert.equal(token.value, 30)
+  assert.equal(averageValue, 30)
+  
+  const token1 = twitch.actionTokenFromMessage('Move the red train forward at power 60', config)
+  twitch.update()
+  assert.equal(averageValue, 45)
+    
+  const token2 = twitch.actionTokenFromMessage('Turn on the lights of the red train', config)
+  twitch.update()
+  assert.equal(averageValue, 100)
+
 }
 
 module.exports = { test }
