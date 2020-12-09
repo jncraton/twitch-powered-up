238f4e91fc5aaef2101df77a906922c10f60cb60
Jon Craton <jncraton@gmail.com>
2020-12-09 13:18:56 -0500

    Add support for action lifetime (#68)
    This change adds support for a lifetime parameter on each device action in addition to the global lifetime parameter. This makes it possible to implement short-lived actions.

--- test/messages.js
+++ test/messages.js
@@ -5,6 +5,7 @@ const messages = require('../src/messages')
 const test = () => {
   messages.setAllowedDevices(config.devices)
 
+  // Averaging values from different users
   messages.add('red train go 50', 'user1')
   assert.equal(messages.getAverageValue('hub1', 'A', 'setPower'), 50)
   messages.add('red train go 100', 'user2')
@@ -13,11 +14,13 @@ const test = () => {
   messages.expire(0)
   assert.equal(messages.getAverageValue('hub1', 'A', 'setPower'), 0)
 
+  // Relative value adjustment
   messages.add('red train go 50', 'user1')
   assert.equal(messages.getAverageValue('hub1', 'A', 'setPower'), 50)
   messages.add('red train faster 50', 'user2')
   assert.equal(messages.getAverageValue('hub1', 'A', 'setPower'), 75)
 
+  // Replacing value from the same user
   messages.expire(0)
   assert.equal(messages.getAverageValue('hub1', 'A', 'setPower'), 0)
 
@@ -25,6 +28,18 @@ const test = () => {
   assert.equal(messages.getAverageValue('hub1', 'A', 'setPower'), 50)
   messages.add('red train faster 50', 'user1')
   assert.equal(messages.getAverageValue('hub1', 'A', 'setPower'), 100)
+
+  // Custom lifetime
+  messages.expire(0)
+  assert.equal(messages.getAverageValue('hub1', 'B', 'setBrightness'), 0)
+
+  messages.add('red lights flash', 'user1')
+  assert.equal(messages.getAverageValue('hub1', 'B', 'setBrightness'), 80)
+
+  setTimeout(() => {
+    messages.expire(1000)
+    assert.equal(messages.getAverageValue('hub1', 'B', 'setBrightness'), 0)
+  }, 10)
 }
 
 module.exports = { test }
